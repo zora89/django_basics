60246b5a9361398f16eb97c3e17d76def44c3837
zora89 <zorawar.purohit@gmail.com>
2022-07-15 03:17:57 +0530

    Early Stage Dev

--- baatcheet/base/views.py
+++ baatcheet/base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+rooms = [
+    {'id':1, 'name': "Lets Discuss Politics"},
+    {'id':2, 'name': "Lets Discuss Science"},
+    {'id':3, 'name': "Lets Discuss History"},
+]
+
+
+def home(request):
+    return render(request, 'home.html', {'rooms' : rooms})
+
+def room(request):
+    return render(request, 'room.html')
+    
